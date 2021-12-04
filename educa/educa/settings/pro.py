f82bbb6a98448c5657298828f0a19f6e4dc21d27
Adewole1 <madedayo@gmail.com>
2021-12-04 03:11:41 +0100

    Getting ready for production

--- educa/educa/settings/pro.py
+++ educa/educa/settings/pro.py
@@ -2,12 +2,21 @@ from .base import *
 
 DEBUG = False
 
+if os.getcwd() == '/app':
+    import dj_database_url
+    DATABASES = {
+    'default': dj_database_url.config(default='postgres://localhost')
+    }
+    # Honor the 'X-Forwarded-Proto' header for request.is_secure().
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    # Allow all host headers.
+    ALLOWED_HOSTS = ['crash-course1.herokuapp.com']
+
 ADMINS = {
     'Adedayo A', 'madedayo@gmail.com'
 }
 
 ALLOWED_HOSTS = ['.crashcourse.com']
-
 
 
 DATABASES = {
